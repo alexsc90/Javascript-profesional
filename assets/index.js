5db6082e2d9c827304dbc416b109720dc3b61d71
Alejandro Silva <a.s.cebreros@gmail.com>
2021-06-15 17:04:49 -0500

    Mute/unmute plugin agregado, práctica de scope y DOM

---
+++ assets/index.js
@@ -0,0 +1,18 @@
+import MediaPlayer from './MediaPlayer';
+import AutoPlay from './plugins/AutoPlay';
+
+const video = document.querySelector("video");
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
+
+const playButton = document.querySelector("#play");
+playButton.onclick = () => player.togglePlay();
+
+const muteButton = document.querySelector("#mute");
+muteButton.onclick = () => {
+    if(player.media.muted) {
+        player.unmute();
+    } else {
+        player.mute();
+    }
+};
+
